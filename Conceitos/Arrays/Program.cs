0864ef7251b8a44e1d3bba396edf0a718494c2e8
Vanessa Pereira <84781224+vanessappereira@users.noreply.github.com>
2025-05-20 12:19:59 +0100

    Added today exercises

--- Conceitos/Arrays/Program.cs
+++ Conceitos/Arrays/Program.cs
@@ -4,10 +4,16 @@
     {
         static void Main(string[] args)
         {
-            // Array - estrutura de dados que armazena um conjunto de elementos do mesmo tipo
-            int[] numeros1; // Declaração de um array de inteiros
+            /* Array - estrutura de dados que armazena um conjunto de elementos do mesmo tipo */
 
-            int[] numeros2 = new int[5]; // Inicialização de um array de inteiros com 5 elementos
+            // Declaração de um array de inteiros
+            int[] numeros1;
+
+            // Inicialização de um array de inteiros com 5 elementos
+            int[] numeros2 = new int[5];
+
+            // Inicialização de um array de inteiros com valores predefinidos
+            int[] numeros3 = { 1, 2, 3, 4, 5 };
 
             // Atribuição de valores ao array numeros2
             numeros2[0] = 10;
@@ -16,10 +22,11 @@
             numeros2[3] = 40;
             numeros2[4] = 50;
 
+            // Random para gerar números aleatórios
             Random rand = new Random();
 
             // Inicializa o array numeros1 com 5 elementos e atribui valores aleatórios
-            numeros1 = new int[5];
+            numeros1 = new int[8];
             for (int i = 0; i < numeros1.Length; i++)
             {
                 numeros1[i] = rand.Next(1, 101); // Atribui um valor aleatório entre 1 e 100
@@ -28,7 +35,7 @@
             // Exibe o tamanho do array
             Console.WriteLine("Tamanho do array numeros1: " + numeros1.Length);
             Console.WriteLine("Tamanho do array numeros2: " + numeros2.Length);
-
+            Console.WriteLine("Tamanho do array numeros3: " + numeros3.Length);
 
             // Exibe os valores do array numeros1
             Console.WriteLine("\nValores do array numeros1:");
@@ -39,9 +46,16 @@
 
             // Exibe os valores do array numeros2
             Console.WriteLine("\nValores do array numeros2:");
-            for (int i = 0; i < numeros2.Length; i++)
+            foreach (int valor2 in numeros2)
             {
-                Console.WriteLine($"numeros2[{i}] = {numeros2[i]}");
+                Console.WriteLine($"numero = {valor2}");
+            }
+
+            // Exibe os valores do array numeros3
+            Console.WriteLine("\nValores do array numeros3:");
+            for (int i = 0; i < numeros3.Length; i++)
+            {
+                Console.WriteLine($"numeros3[{i}] = {numeros3[i]}");
             }
         }
     }
